5d13f2c0baa730e8df919545abc77955d8ab6229
mohamed <mmdbadawy745@gmail.com>
2026-07-25 23:29:24 +0300

    first commit

---
+++ lib/dart_application_1.dart
@@ -0,0 +1,73 @@
+
+
+
+void main(){
+ print('MyName'); // Moahmed Badawy
+   
+}
+
+
+
+
+/*question no.1
+for(int i=1; i<=20;i++){
+print(i);
+}
+*/
+
+
+/*question no.2
+List food=["shawerma","molokhia","pizza","seafood","burger"];
+    for(String item in food ){
+        print(item);
+}
+*/
+
+
+/*question no.3
+ List<int> numbers =[1,2,3,4,5,6,7,8,9,10];
+    for(int i=0 ; i<numbers.length ;i++){
+      if(numbers[i].isEven)
+     print(numbers[i]);
+    }
+*/
+
+
+/*question no.4
+String week='monday';
+
+switch(week.toLowerCase()){
+    case 'monday' : print(1);
+    case'tuesday' : print(2);
+    case'wednesday' : print(3);
+    case'thursday' : print(4);
+    case'friday' : print(5);
+    case'saturday' : print(6);
+    case'sunday' : print(7);
+    default : print('error');
+}
+*/   
+/* question no.5
+i dont know the answer 
+*/
+/*question no.6
+ int counter=0;
+  while (counter<10)
+  {
+    print("Hello");
+    counter++;
+  }
+*/
+/*question no.7
+List studentNames=["Ahmed","Moahmed","Maged","Waheed","Rami"];
+ 
+ String studentNames="Ahmed";
+ switch(studentNames.toLowerCase()){
+  case"Ahmed" : print('firstName');
+  case"Mohamed" : print('secondName');
+  case"Maged" : print('thirdName');
+  case"Waheed" : print('forthName');
+  case"Rami" : print('fifthName');
+  default: print('error');
+ }
+ */ 
